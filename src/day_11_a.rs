4d31dd51cfbb4bc6041edfd5e976799ac33f7be0
daemon <0xDAEF0F@proton.me>
2023-10-31 11:22:57 -0600

    day 11 middle

--- src/day_11_a.rs
+++ src/day_11_a.rs
@@ -1,7 +1,9 @@
 struct Monkey {
     items: Vec<u32>,
-    throw_idx_if_true: u8,
-    throw_idx_if_false: u8,
+    monkey_index: usize,
+    inspected_items: u32,
+    throw_idx_if_true: usize,
+    throw_idx_if_false: usize,
 }
 
 impl Monkey {
@@ -19,7 +21,7 @@ impl Monkey {
         }
     }
 
-    fn who_to_throw(monkey_idx: u8, flag: bool) -> u8 {
+    fn who_to_throw(monkey_idx: u8, flag: bool) -> usize {
         match monkey_idx {
             0 => match flag {
                 true => 6,
@@ -56,14 +58,72 @@ impl Monkey {
             _ => panic!("must be from 0..=7"),
         }
     }
+
+    fn execute_monkey_turn(&mut self, monkeys: &mut Vec<Monkey>) {
+        for item in self.items.iter() {
+            //
+        }
+    }
+
+    fn get_new_worry_level(&self, worry_level: u32) -> u32 {
+        match self.monkey_index {
+            0 => worry_level * 7,
+            1 => worry_level * 11,
+            2 => worry_level + 8,
+            3 => worry_level + 7,
+            4 => worry_level + 5,
+            5 => worry_level + 4,
+            6 => worry_level * worry_level,
+            7 => worry_level + 3,
+            _ => panic!("Monkey does not exist!"),
+        }
+    }
+
+    fn throw_item_to(&self, worry_level: u32) -> bool {
+        match self.monkey_index {
+            0 => worry_level % 19 == 0,
+            1 => worry_level % 3 == 0,
+            2 => worry_level % 13 == 0,
+            3 => worry_level % 7 == 0,
+            4 => worry_level % 5 == 0,
+            5 => worry_level % 11 == 0,
+            6 => worry_level % 17 == 0,
+            7 => worry_level % 2 == 0,
+            _ => panic!("Monkey does not exist!"),
+        }
+    }
 }
 
 pub fn day_11_a(str: String) {
-    let monkeys: Vec<Monkey> = (0..=7)
-        .map(|i| Monkey {
-            items: Monkey::get_initial_items(i),
-            throw_idx_if_true: Monkey::who_to_throw(i, true),
-            throw_idx_if_false: Monkey::who_to_throw(i, false),
+    let mut monkeys: Vec<Monkey> = (0..=7)
+        .map(|i| {
+            let items = Monkey::get_initial_items(i);
+            Monkey {
+                items,
+                monkey_index: i as usize,
+                inspected_items: 0,
+                throw_idx_if_true: Monkey::who_to_throw(i, true),
+                throw_idx_if_false: Monkey::who_to_throw(i, false),
+            }
         })
         .collect();
+
+    for _ in 0..=20 {
+        for monkey in 0..monkeys.len() {
+            // iterate over the monkey's items
+            let a = monkey.items.iter().map(|item| {
+                let new_worry_level = monkey.get_new_worry_level(*item);
+                let throw_to = match monkey.throw_item_to(new_worry_level) {
+                    true => monkey.throw_idx_if_true,
+                    false => monkey.throw_idx_if_false,
+                };
+
+                (new_worry_level, throw_to)
+            });
+
+            for (item, to) in a {
+                &monkeys[to].items.push(item);
+            }
+        }
+    }
 }
